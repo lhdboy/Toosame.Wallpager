9351c5a243653d3ec3e0889b592473f306fcfdcf
旋风小伙 <lhdboy@outlook.com>
2018-09-10 18:42:24 +0800

    添加pic组件

---
+++ src/Toosame.Wallpager/ClientApp/src/components/pic/index.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+
+export class TagButton extends Component {
+    displayName = TagButton.name
+
+    render() {
+        return (
+            <ListGroup>
+                <ListGroupItem header="Heading 1">Some body text</ListGroupItem>
+                <ListGroupItem header="Heading 2" href="#">
+                    Linked item
+                </ListGroupItem>
+                <ListGroupItem header="Heading 3" bsStyle="danger">
+                    Danger styling
+                </ListGroupItem>
+            </ListGroup>
+        );
+    }
+}
